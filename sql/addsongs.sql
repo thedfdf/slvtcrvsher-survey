5ef7ccbbc5b0b1164f02f04f70a1fbe1c32092f3
dfdf <129122952+thedfdf@users.noreply.github.com>
2023-05-25 19:38:24 -0500

    Create addsomesongs.sql
    just exmaple data

---
+++ sql/addsongs.sql
@@ -0,0 +1,11 @@
+INSERT INTO songs (Title, Album, Rating) VALUES
+  ('Interdimensional Snuff Films', 'Interdimensional Snuff Films', 0),
+  ('Flaminroids (Crouching Shrim Sewerslvt Remix)', 'Interdimensional Snuff Films', 0),
+  ('Warping (Sewerslvt Remix)', 'Interdimensional Snuff Films', 0),
+  ('2Strings (Sewerslvt Movement Remix)', 'Interdimensional Snuff Films', 0),
+  ('Kvltvre Shock', 'Interdimensional Snuff Films', 0),
+  ('World of Dogs (Sewerslvt Remix)', 'Interdimensional Snuff Films', 0),
+  ('Ain''t It Funny (Sewerslvt Remix)', 'Interdimensional Snuff Films', 0),
+  ('Love Taste (Sewerslvt Memory Remix)', 'Interdimensional Snuff Films', 0),
+  ('Bottomless Pit (Sewerslvt Sega Remix)', 'Interdimensional Snuff Films', 0),
+  ('Hotline Spikes (Sewerslvt Remake)', 'Interdimensional Snuff Films', 0);
